6a8edc8d96786dbecda59f7c90c58053a6eeb714
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2021-10-18 19:52:42 +0200

    Ajout du footer

--- statics/footer.php
+++ statics/footer.php
@@ -1,5 +1,55 @@
-<footer>
+<footer class="text-center text-lg-start bg-light text-muted">
 
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i><?php $siteName ?>
+                    </h6>
+                    <p>
+                        Site réalisé pour l'UE BDW1 du semestre d'automne de l'année 2021/2022
+                    </p>
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Scolarité
+                    </h6>
+                    <p>
+                        <a href="https://www.univ-lyon1.fr" class="text-reset">Université Claude Bernard Lyon 1</a>
+                    </p>
+                    <p>
+                        <a href="https://perso.liris.cnrs.fr/fabien.duchateau/BDW1/" class="text-reset">BDW1</a>
+                    </p>
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Remerciements
+                    </h6>
+                    <p>
+                        <a href="https://getbootstrap.com" class="text-reset">Bootstrap</a>
+                    </p>
+                    <p>
+                        <a href="https://www.php.net" class="text-reset">PHP</a>
+                    </p>
+                    <p>
+                        <a href="https://mariadb.org" class="text-reset">MariaDB</a>
+                    </p>
+                </div>
+
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Contacts
+                    </h6>
+                    <p><i class="me-3"></i>SIONI Farès - p1907037</p>
+                    <p><i class="me-3"></i>MOKADEM Aziz - p</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
 
 
