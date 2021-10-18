354c854652e3eadb23b359afbc497ee018949af0
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2021-10-18 19:42:09 +0200

    Ajout Bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 // SIONI Farès - p1907037
 // MOKADEM Aziz
-//scp -r serial-critique p1907037@bdw1.univ-lyon1.fr:/var/www/p1907037/
+//scp -r bdw1-projet p1907037@bdw1.univ-lyon1.fr:/var/www/p1907037/
 
 $title = 'Accueil';
 require_once 'statics/header.php';
+require_once 'statics/nav.php';
 ?>
 
-
+<h1>Hello, world</h1>
 
 <?php require_once 'statics/footer.php' ?>
