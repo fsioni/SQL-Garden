941420c9eaab186a904971046b10547b1052939c
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2021-11-08 18:45:40 +0100

    Ajout du formulaire d'ajout de variété

---
+++ views/viewAjouterVariete.php
@@ -0,0 +1,73 @@
+    <h1>Ajouter une variété</h1>
+    <br>
+
+    <form>
+        <div class="form-group">
+            <label for="nomVariete">Nom</label>
+            <input type="text" class="form-control" placeholder="Fraisier des bois">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Année d'enregistrement</label>
+            <input type="number" min="1900" max="2022" step="1" value="2021" class="form-control">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Code précocité</label>
+            <input type="text" class="form-control" placeholder="S1">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Plantation</label>
+            <input type="text" class="form-control" placeholder="S1">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Entretien</label>
+            <input type="text" class="form-control" placeholder="S1">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Récolte</label>
+            <input type="text" class="form-control" placeholder="S1">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Jours levée</label>
+            <input type="text" class="form-control" placeholder="S1">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Période plantation</label>
+            <input type="text" class="form-control" placeholder="S1">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Période récolte</label>
+            <input type="text" class="form-control" placeholder="S1">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Commentaire général</label>
+            <input type="text" class="form-control" placeholder="S1">
+        </div>
+        <br>
+
+        <div class="form-group">
+            <label for="nomVariete">Descriptions pour le semis</label> <br>
+            <small class="form-text text-muted">Séparez les descriptions par des points virgules.</small>
+            <input type="text" class="form-control" placeholder="S1">
+        </div>
+        <br>
+
+        <button type="submit" class="btn btn-primary">Soumettre</button>
+    </form>
