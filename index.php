fcd9014fc5e5c91acfd70d8d74e5e7e56ffa3173
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2021-11-22 17:26:49 +0100

    Amélioration du MVC

--- index.php
+++ index.php
@@ -3,20 +3,6 @@
 // MOKADEM Aziz
 //scp -r bdw1-projet p1907037@bdw1.univ-lyon1.fr:/var/www/p1907037/
 
-$controller = 'controllerAccueil'; // contrôleur par défaut
-$view = 'viewAccueil'; // vue par défaut
-
-require_once('includes/routes.php');
-
-if (isset($_GET['page'])) {
-    $nomPage = $_GET['page'];
-    if (isset($routes[$nomPage])) {
-        $controller = $routes[$nomPage]['controller'];
-        $view = $routes[$nomPage]['view'];
-    }
-}
-include('controllers/' . $controller . '.php');
-
 require_once 'statics/header.php';
 require_once 'statics/nav.php';
 
--- statics/header.php
+++ statics/header.php
@@ -4,12 +4,26 @@ session_start(); // démarre ou reprend une session
 ini_set('display_errors', 1); // affiche les erreurs (au cas où)
 ini_set('display_startup_errors', 1); // affiche les erreurs (au cas où)
 
-require('includes/config-db.php'); // inclu laconfig de la bdd
-require('models/model.php'); // inclut le fichier modele
-require('includes/includes.php'); // inclut des constantes et fonctions du site
-require('includes/routes.php'); // fichiers de routes
+require_once('models/model.php'); // inclut le fichier model
+require_once('includes/includes.php'); // inclut des constantes et fonctions du site (nom, slogan)
+require_once('includes/routes.php'); // fichiers de routes
+
+require('includes/config-db.php'); // inclus la config de la bdd
+
+$controller = 'controllerAccueil'; // contrôleur par défaut
+$view = 'viewAccueil'; // vue par défaut
 
 $connexion = getConnexionDB(); // connexion à la BD
+
+if (isset($_GET['page'])) {
+    $nomPage = $_GET['page'];
+    if (isset($routes[$nomPage])) {
+        $controller = $routes[$nomPage]['controller'];
+        $view = $routes[$nomPage]['view'];
+    }
+}
+include('controllers/' . $controller . '.php');
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -20,17 +34,23 @@ $connexion = getConnexionDB(); // connexion à la BD
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="data/css/styles.css">
-    <title><?php echo $siteName . ' - ' . $title; ?></title>
+    <link rel="icon" href="data/img/icon.ico">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+
+
+    <title><?php echo $title . ' - ' . $siteName; ?></title>
 </head>
 
 <body class="bg-light">
-
     <!-- Page Wrapper -->
     <header>
         <div style="background: url(data/img/header.jpeg)" class="jumbotron text-white">
             <div class="container py-5 text-center">
                 <a href="index.php">
-                    <img class="img-fluid" src="https://via.placeholder.com/350x150" alt="Logo <?php echo $siteName ?>">
+                    <img class="img-fluid" src="data/img/banner.jpeg" alt="Logo <?php echo $siteName ?>">
                 </a>
                 <p class="font-italic mb-0" style="margin: 2%;">
                     <?php echo $siteDesc ?>
