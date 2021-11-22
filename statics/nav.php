0520d5b4ab8fae768271bd83bebb133a81745bf5
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2021-11-22 17:27:25 +0100

    Ajout page AfficherVariétés

--- statics/nav.php
+++ statics/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
-    <a class="navbar-brand" href="#" style="margin-left: 1%;"><?php echo $siteName ?></a>
+    <a class="navbar-brand" href="index.php" style="margin-left: 1%;"><?php echo $siteName ?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -7,6 +7,9 @@
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" href="index.php">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=afficher-varietes">Afficher les variétés</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="index.php?page=ajouter-variete">Ajouter une variété</a>
