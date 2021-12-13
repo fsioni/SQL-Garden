587bbce1d4fc223b540a44283d7cc7ca2f7098a5
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2021-12-13 02:30:35 +0100

    Ajout section "gestion jardin"

--- views/viewAccueil.php
+++ views/viewAccueil.php
@@ -1,25 +1,48 @@
     <h1 class="display-5">L'outil de gestion de jardin n°1</h1>
 
     <div class="px-4 py-5 my-5 text-center">
-        <img class="d-block mx-auto mb-4 img-thumbnail" src="data/img/indexImg1.jpg" alt="Photo d'un champ de blé" width="426" height="320">
+        <img class="d-block mx-auto mb-4 img-thumbnail" src="data/img/indexImg1.jpg" alt="Photo d'un champ de blé"
+            width="426" height="320">
         <h1 class="display-5 fw-bold">La gestion de vos variétés</h1>
         <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">Ajoutez et affichez toutes les variétés de plantes. Celles-ci peuvent ensuite être utilisée des vos parcelles de jardins afin de les peupler.</p>
+            <p class="lead mb-4">Ajoutez et affichez toutes les variétés de plantes. Celles-ci peuvent ensuite être
+                utilisée des vos parcelles de jardins afin de les peupler.</p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" onclick="location.href='index.php?page=afficher-varietes'" class="btn btn-primary btn-lg px-4 gap-3">Afficher les variétés</button>
-                <button type="button" onclick="location.href='index.php?page=ajouter-variete'" class="btn btn-outline-secondary btn-lg px-4">Ajouter une variété</button>
+                <button type="button" onclick="location.href='index.php?page=afficher-varietes'"
+                    class="btn btn-primary btn-lg px-4 gap-3">Afficher les variétés</button>
+                <button type="button" onclick="location.href='index.php?page=ajouter-variete'"
+                    class="btn btn-outline-secondary btn-lg px-4">Ajouter une variété</button>
             </div>
         </div>
     </div>
     <hr />
 
     <div class="px-4 py-5 my-5 text-center">
-        <img class="d-block mx-auto mb-4 img-thumbnail" src="data/img/indexImg2.jpg" alt="Photo d'une parcelle de champ" width="426" height="320">
+        <img class="d-block mx-auto mb-4 img-thumbnail" src="data/img/indexImg2.jpg" alt="Photo d'une parcelle de champ"
+            width="426" height="320">
         <h1 class="display-5 fw-bold">Créez les parcelles de vos rêves</h1>
         <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">Avec les paramètres de vos choix, générez aléatoirement des parcelles, composée des variétés et des plantes sauvages de la base de données.</p>
+            <p class="lead mb-4">Avec les paramètres de vos choix, générez aléatoirement des parcelles, composée des
+                variétés et des plantes sauvages de la base de données.</p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" onclick="location.href='index.php?page=generer-parcelle'" class="btn btn-primary btn-lg px-4 gap-3">Générer et afficher une parcelle</button>
+                <button type="button" onclick="location.href='index.php?page=generer-parcelle'"
+                    class="btn btn-primary btn-lg px-4 gap-3">Générer et afficher une parcelle</button>
+            </div>
+        </div>
+    </div>
+
+    <hr />
+
+    <div class="px-4 py-5 my-5 text-center">
+        <img class="d-block mx-auto mb-4 img-thumbnail" src="data/img/indexImg3.jpg" alt="Photo d'un jardin" width="426"
+            height="320">
+        <h1 class="display-5 fw-bold">Gérez vos jardins d'une main de maître</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">Ayez toutes les informations sur vos jardins, parcelles, récoltes, rangs. Procédez aux
+                modiffications de vos choix ou optez pour la suppression de données.</p>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <button type="button" onclick="location.href='index.php?page=gestion-jardin'"
+                    class="btn btn-primary btn-lg px-4 gap-3">Gérer vos jardins</button>
             </div>
         </div>
     </div>
