f6e21b9ffd260b932845f6c574a611068cccc076
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2021-12-13 18:06:16 +0100

    Indentation etc

--- models/scriptCleanse.php
+++ models/scriptCleanse.php
@@ -1,14 +1,12 @@
 <?php
 
-   $connection = mysqli_connect("localhost", "p1804157", "Causal83Petite", "p1804157");
-   $connection->query('SET foreign_key_checks = 0');
-   if ($result = $connection->query("SHOW TABLES"))
-   {
-      while($row = $result->fetch_array(MYSQLI_NUM))
-      {
-         $connection->query('DROP TABLE IF EXISTS '.$row[0]);
-      }
+$connection = mysqli_connect("localhost", "p1804157", "Causal83Petite", "p1804157");
+$connection->query('SET foreign_key_checks = 0');
+if ($result = $connection->query("SHOW TABLES")) {
+   while ($row = $result->fetch_array(MYSQLI_NUM)) {
+      $connection->query('DROP TABLE IF EXISTS ' . $row[0]);
    }
+}
 
-   $connection->query('SET foreign_key_checks = 1');
-   $connection->close();
+$connection->query('SET foreign_key_checks = 1');
+$connection->close();
--- models/scriptSetDatabase.php
+++ models/scriptSetDatabase.php
@@ -1,28 +1,24 @@
 <?php
 
-   $connection = mysqli_connect("localhost","p1804157","Causal83Petite","p1804157");
-   if (mysqli_connect_errno()) {
-        printf("Échec de la connexion : %s\n", mysqli_connect_error());
-        exit();
-    }
+$connection = mysqli_connect("localhost", "p1804157", "Causal83Petite", "p1804157");
+if (mysqli_connect_errno()) {
+	printf("Échec de la connexion : %s\n", mysqli_connect_error());
+	exit();
+}
 
-   $sqlScript = file('../create.sql');
-   foreach ($sqlScript as $line)	{
+$sqlScript = file('../create.sql');
+foreach ($sqlScript as $line) {
 
-	   $startWith = substr(trim($line), 0 ,2);
-	   $endWith = substr(trim($line), -1 ,1);
+	$startWith = substr(trim($line), 0, 2);
+	$endWith = substr(trim($line), -1, 1);
 
-	   if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//') {
-		   continue;
-	   }
+	if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//') {
+		continue;
+	}
 
-	   $query = $query . $line;
-	   if ($endWith == ';') {
-		   mysqli_query($connection,$query) or die('<div class="error-response sql-import-response">Problem in executing the SQL query <b>' . $query. '</b></div>');
-		   $query= '';		
-	   }
-   }
-
-?>
-
-
+	$query = $query . $line;
+	if ($endWith == ';') {
+		mysqli_query($connection, $query) or die('<div class="error-response sql-import-response">Problem in executing the SQL query <b>' . $query . '</b></div>');
+		$query = '';
+	}
+}
